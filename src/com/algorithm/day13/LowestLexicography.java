df561420b581565f8929eeaf7e3bc9f493e621b6
peter <1042397946@qq.com>
2021-05-07 15:52:17 +0800

    二叉树递归套路练习

---
+++ src/com/algorithm/day13/LowestLexicography.java
@@ -0,0 +1,11 @@
+package com.algorithm.day13;
+
+/**
+ * @Author ycb
+ * @Date 2021/5/7-14:33
+ * @Description 贪心算法
+ * 给定一个由字符串组成的数组strs，必须把所有的字符串拼接起来，返回所有可能的拼接结果中，字典序最小的结果
+ */
+public class LowestLexicography {
+
+}
---
+++ src/com/algorithm/day13/MaxSubBSTHead.java
@@ -0,0 +1,9 @@
+package com.algorithm.day13;
+
+/**
+ * @Author ycb
+ * @Date 2021/5/7-14:32
+ * @Description 给定一棵二叉树的头节点head，返回这颗二叉树中最大的二叉搜索子树的头节点
+ */
+public class MaxSubBSTHead {
+}
