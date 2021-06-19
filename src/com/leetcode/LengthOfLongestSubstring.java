8d5edff3912d9253f8afecc6e1a13317923f67c6
peter <1042397946@qq.com>
2021-06-19 16:53:48 +0800

    数据三连 + 矩阵分圈技巧

--- src/com/leetcode/LengthOfLongestSubstring.java
+++ src/com/leetcode/LengthOfLongestSubstring.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.HashMap;
+
 /**
  * @Author ycb
  * @Date 2021/3/12-9:47
@@ -7,10 +9,19 @@ package com.leetcode;
  */
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0){
+        if (s == null || s.length() == 0) {
             return 0;
         }
-        char[] str = s.toCharArray();
-        return 0;
+        int max = 0; // 记录当前窗口内最长的无重复子串长度
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
     }
 }
