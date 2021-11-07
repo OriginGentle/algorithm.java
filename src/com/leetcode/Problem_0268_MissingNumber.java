4db12c98f92d440be72689bfc003c31d6e47c26d
peter <1042397946@qq.com>
2021-11-07 13:29:00 +0800

    day44

---
+++ src/com/leetcode/Problem_0268_MissingNumber.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2021/11/6-17:32
+ */
+public class Problem_0268_MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = (n * (n + 1)) >> 1;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return total - sum;
+    }
+}
