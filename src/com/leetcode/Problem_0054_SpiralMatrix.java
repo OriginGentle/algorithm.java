ea37bb4edc2d87c0c59e02de2e7f153afb790be0
peter <1042397946@qq.com>
2021-12-15 08:19:00 +0800

    leetcode59

--- src/com/leetcode/Problem_0054_SpiralMatrix.java
+++ src/com/leetcode/Problem_0054_SpiralMatrix.java
@@ -11,44 +11,60 @@ public class Problem_0054_SpiralMatrix {
 
     public static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
-        int tR = 0;
-        int tC = 0;
-        int dR = matrix.length - 1;
-        int dC = matrix[0].length - 1;
-        while (tR <= dR && tC <= dC) {
-            printEdge(matrix, tR++, tC++, dR--, dC--, ans);
+        int tRow = 0;
+        int tCol = 0;
+        int dRow = matrix.length - 1;
+        int dCol = matrix[0].length - 1;
+        while (tRow <= dRow && tCol <= dCol) {
+            printEdge(matrix, tRow++, tCol++, dRow--, dCol--, ans);
         }
         return ans;
     }
 
-    public static void printEdge(int[][] m, int tR, int tC, int dR, int dC, List<Integer> ans) {
-        if (tR == dR) {
-            for (int i = tC; i <= dC; i++) {
-                ans.add(m[tR][i]);
+    public static void printEdge(int[][] m, int tRow, int tCol, int dRow, int dCol, List<Integer> ans) {
+        if (tRow == dRow) {
+            for (int i = tCol; i <= dCol; i++) {
+                ans.add(m[tRow][i]);
             }
-        } else if (tC == dC) {
-            for (int i = tR; i <= dR; i++) {
-                ans.add(m[i][tC]);
+        } else if (tCol == dCol) {
+            for (int i = tRow; i <= dRow; i++) {
+                ans.add(m[i][tCol]);
             }
         } else {
-            int curC = tC;
-            int curR = tR;
-            while (curC != dC) {
-                ans.add(m[tR][curC]);
+            int curC = tCol;
+            int curR = tRow;
+            while (curC != dCol) {
+                ans.add(m[tRow][curC]);
                 curC++;
             }
-            while (curR != dR) {
-                ans.add(m[curR][dC]);
+            while (curR != dRow) {
+                ans.add(m[curR][dCol]);
                 curR++;
             }
-            while (curC != tC) {
-                ans.add(m[dR][curC]);
+            while (curC != tCol) {
+                ans.add(m[dRow][curC]);
                 curC--;
             }
-            while (curR != tR) {
-                ans.add(m[curR][tC]);
+            while (curR != tRow) {
+                ans.add(m[curR][tCol]);
                 curR--;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] matrix = new int[n][n];
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = count++;
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("=======================");
+        List<Integer> list = spiralOrder(matrix);
+        list.forEach(System.out::print);
     }
 }
