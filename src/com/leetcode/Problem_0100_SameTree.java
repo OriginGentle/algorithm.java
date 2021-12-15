e22ddfa3b766d4d195df8045643d79a8f629e1ee
peter <1042397946@qq.com>
2021-12-15 14:55:51 +0800

    leetcode60

---
+++ src/com/leetcode/Problem_0100_SameTree.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2021/12/15-8:21
+ */
+public class Problem_0100_SameTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null ^ q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
