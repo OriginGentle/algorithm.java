d4a5d407fabee6e2ae4e615da3e066b4fb046a17
peter <1042397946@qq.com>
2022-01-20 14:51:05 +0800

    每日一题

---
+++ src/com/leetcode/Problem_2029_StoneGameIX.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2022/1/20-8:22
+ */
+public class Problem_2029_StoneGameIX {
+
+    public static boolean stoneGameIX(int[] stones) {
+        int[] tmp = new int[3];
+        for (int i : stones) {
+            tmp[i % 3]++;
+        }
+        return tmp[0] % 2 == 0 ? !(tmp[1] == 0 || tmp[2] == 0) : !(Math.abs(tmp[1] - tmp[2]) <= 2);
+    }
+}
