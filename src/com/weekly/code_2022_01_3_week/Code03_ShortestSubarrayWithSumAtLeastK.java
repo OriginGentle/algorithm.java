586bb005384ea20472d344cf524b7b4cf8968b6b
peter <1042397946@qq.com>
2022-01-23 17:06:36 +0800

    weekly

---
+++ src/com/weekly/code_2022_01_3_week/Code03_ShortestSubarrayWithSumAtLeastK.java
@@ -0,0 +1,80 @@
+package com.weekly.code_2022_01_3_week;
+
+/**
+ * @author ycb
+ * @date 2022/1/23-16:34
+ * @description 来自字节跳动
+ * 给定一个数组arr，其中的值有可能正、负、0
+ * 给定一个正数k
+ * 返回累加和>=k的所有子数组中，最短的子数组长度
+ * <p>
+ * https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+ */
+public class Code03_ShortestSubarrayWithSumAtLeastK {
+
+    public static int shortestSubarray1(int[] arr, int k) {
+        if (arr == null || arr.length < 1) {
+            return -1;
+        }
+        int n = arr.length + 1;
+        long[] sum = new long[n];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + arr[i - 1];
+        }
+        int[] stack = new int[n];
+        int size = 1;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int mostRight = mostRight(sum, stack, size, sum[i] - k);
+            ans = Math.min(ans, mostRight == -1 ? Integer.MAX_VALUE : (i - mostRight));
+            while (size > 0 && sum[stack[size - 1]] >= sum[i]) {
+                size--;
+            }
+            stack[size++] = i;
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public static int mostRight(long[] sum, int[] stack, int size, long aim) {
+        int l = 0;
+        int r = size - 1;
+        int m = 0;
+        int ans = -1;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (sum[stack[m]] <= aim) {
+                ans = stack[m];
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+
+    /*
+    ====================================================================================================================
+     */
+
+    public static int shortestSubarray2(int[] arr, int k) {
+        int N = arr.length;
+        long[] sum = new long[N + 1];
+        for (int i = 0; i < N; i++) {
+            sum[i + 1] = sum[i] + arr[i];
+        }
+        int ans = Integer.MAX_VALUE;
+        int[] dp = new int[N + 1];
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < N + 1; i++) {
+            while (l != r && sum[i] - sum[dp[l]] >= k) {
+                ans = Math.min(ans, i - dp[l++]);
+            }
+            while (l != r && sum[dp[r - 1]] >= sum[i]) {
+                r--;
+            }
+            dp[r++] = i;
+        }
+        return ans != Integer.MAX_VALUE ? ans : -1;
+    }
+}
