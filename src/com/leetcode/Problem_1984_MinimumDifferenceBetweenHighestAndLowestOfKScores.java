93294debcc2148f322b51eba40224b2b7bc42f32
peter <1042397946@qq.com>
2022-02-12 21:10:38 +0800

    weekly

---
+++ src/com/leetcode/Problem_1984_MinimumDifferenceBetweenHighestAndLowestOfKScores.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ycb
+ * @since 2022/2/11-18:48
+ */
+public class Problem_1984_MinimumDifferenceBetweenHighestAndLowestOfKScores {
+
+    public static int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = nums[k - 1] - nums[0];
+        for (int i = k; i < n; i++) {
+            ans = Math.min(ans, nums[i] - nums[i - k + 1]);
+        }
+        return ans;
+    }
+}
---
+++ src/com/weekly/code_2022_02_2_week/Code03_FindKthSmallestPairDistance.java
@@ -0,0 +1,42 @@
+package com.weekly.code_2022_02_2_week;
+
+import java.util.Arrays;
+
+/**
+ * @author ycb
+ * @date 2022/2/11-18:22
+ * @description https://leetcode.com/problems/find-k-th-smallest-pair-distance/
+ */
+public class Code03_FindKthSmallestPairDistance {
+
+    public static int smallestDistancePair(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums[n - 1] - nums[0];
+        int ans = 0;
+        while (l <= r) {
+            int dis = l + ((r - l) >> 1);
+            int cnt = process(nums, dis);
+            if (cnt >= k) {
+                ans = dis;
+                r = dis - 1;
+            } else {
+                l = dis + 1;
+            }
+        }
+        return ans;
+    }
+
+    // <= dis的数字对，有多少个，返回
+    private static int process(int[] arr, int dis) {
+        int cnt = 0;
+        for (int l = 0, r = 0; l < arr.length; l++) {
+            while (r < arr.length && arr[r] <= arr[l] + dis) {
+                r++;
+            }
+            cnt += r - l - 1;
+        }
+        return cnt;
+    }
+}
