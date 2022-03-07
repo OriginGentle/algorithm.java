b0e933f9d8f6d06594d80997d5d9a0e3343c72bc
peter <1042397946@qq.com>
2022-03-07 11:40:51 +0800

    每日一题

--- src/com/leetcode/Problem_0504_Base7.java
+++ src/com/leetcode/Problem_0504_Base7.java
@@ -6,8 +6,22 @@ package com.leetcode;
  */
 public class Problem_0504_Base7 {
 
-    public String convertToBase7(int num) {
+    public static String convertToBase7(int num) {
+        if (num == 0) {
+            return String.valueOf(num);
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean flag = num < 0;
+        num = Math.abs(num);
+        while (num != 0) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+        return flag ? "-" + sb.reverse().toString() : sb.reverse().toString();
+    }
 
-        return null;
+    public static void main(String[] args) {
+        int num = 100;
+        System.out.println(convertToBase7(num));
     }
 }
