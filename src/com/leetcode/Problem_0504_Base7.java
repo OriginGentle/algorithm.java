7183935c43aa100eed296daa85a430b8e8e6616a
yangchangbin <1042397946@qq.com>
2022-03-07 08:48:31 +0800

    每日一题

---
+++ src/com/leetcode/Problem_0504_Base7.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2022/3/7-8:46
+ */
+public class Problem_0504_Base7 {
+
+    public String convertToBase7(int num) {
+
+        return null;
+    }
+}
