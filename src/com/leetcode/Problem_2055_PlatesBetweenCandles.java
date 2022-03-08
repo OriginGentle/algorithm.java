c733ede9e5357412409429b01195bfe3f9d01215
peter <1042397946@qq.com>
2022-03-08 10:55:44 +0800

    每日一题

--- src/com/leetcode/Problem_2055_PlatesBetweenCandles.java
+++ src/com/leetcode/Problem_2055_PlatesBetweenCandles.java
@@ -13,22 +13,22 @@ public class Problem_2055_PlatesBetweenCandles {
         int[] preSum = new int[n];
         int[] left = new int[n];
         int[] right = new int[n];
-        int[] ans = new int[n];
+        int[] ans = new int[m];
         int sum = 0;
         for (int i = 0, j = n - 1, p = -1, q = -1; i < n; i++, j--) {
             if (s.charAt(i) == '|') p = i;
             if (s.charAt(j) == '|') q = j;
             left[i] = p;
             right[j] = q;
-            if (s.charAt(i) == '*') preSum[i] = ++sum;
+            sum = sum + (s.charAt(i) == '*' ? 1 : 0);
+            preSum[i] = sum;
         }
         for (int i = 0; i < m; i++) {
-            // 左侧蜡烛的位置
-            int l = left[queries[i][0]];
-            // 右侧蜡烛的位置
-            int r = right[queries[i][1]];
-            // 左侧得有蜡烛
-            if (l != -1 && r <= l) ans[i] = preSum[r] - preSum[l];
+            // 起始位置往右第一跟蜡烛的位置
+            int posR = right[queries[i][0]];
+            // 结束位置往左第一根蜡烛的位置
+            int posL = left[queries[i][1]];
+            ans[i] = posR == -1 || posL == -1 || posR >= posL ? 0 : preSum[posL] - preSum[posR];
         }
         return ans;
     }
@@ -37,6 +37,8 @@ public class Problem_2055_PlatesBetweenCandles {
         String s = "**|**|***|";
         int[][] q = {{2, 5}, {5, 9}};
         int[] ans = platesBetweenCandles(s, q);
-        System.out.println();
+        for (int an : ans) {
+            System.out.println(an);
+        }
     }
 }
