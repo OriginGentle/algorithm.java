c914c264dbfcd3b5a07b114360fd0dde942c9dcc
peter <1042397946@qq.com>
2022-03-09 10:38:06 +0800

    每日一题

---
+++ src/com/leetcode/Problem_0107_BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,59 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * @author ycb
+ * @since 2022/3/9-10:23
+ */
+public class Problem_0107_BinaryTreeLevelOrderTraversalII {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int level = 0;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curAns = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                curAns.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            map.put(level++, curAns);
+        }
+        for (int i = level - 1; i >= 0; i--) {
+            ans.add(map.get(i));
+        }
+        return ans;
+    }
+}
