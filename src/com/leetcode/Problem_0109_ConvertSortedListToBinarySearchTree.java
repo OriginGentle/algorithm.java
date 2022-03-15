6cf4e12b8a10851e734bb571023fb802ea9c2049
peter <1042397946@qq.com>
2022-03-15 17:03:32 +0800

    每日一题

---
+++ src/com/leetcode/Problem_0109_ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2022/3/15-10:02
+ */
+public class Problem_0109_ConvertSortedListToBinarySearchTree {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+
+        return null;
+    }
+
+}
