056395d2def01954b1a87b475a99f6794b65739a
peter <1042397946@qq.com>
2022-03-17 13:59:31 +0800

    简单训练

---
+++ src/com/leetcode/Problem_1523_CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @since 2022/3/17-13:45
+ */
+public class Problem_1523_CountOddNumbersInAnIntervalRange {
+
+    public int countOdds(int low, int high) {
+        return ((high - low) / 2) + ((low & 1) | (high & 1));
+    }
+}
