0d749f919caefd932ddaabb8c32f7a8b0fe5f877
yangchangbin <1042397946@qq.com>
2022-03-20 20:58:36 +0800

    简单入门训练

---
+++ src/com/leetcode/Problem_1790_CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @date 2022/3/20-20:47
+ */
+public class Problem_1790_CheckIfOneStringSwapCanMakeStringsEqual {
+
+    public boolean areAlmostEqual(String s1, String s2) {
+        char c1 = '#', c2 = '#';
+        boolean hasChange = false;
+        int n = s1.length();
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (hasChange) {
+                    return false;
+                } else if (c1 == '#') {
+                    c1 = s1.charAt(i);
+                    c2 = s2.charAt(i);
+                } else {
+                    if (c1 == s2.charAt(i) && c2 == s1.charAt(i)) {
+                        hasChange = true;
+                        c2 = '#';
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return c2 == '#';
+    }
+}
---
+++ src/com/leetcode/Problem_1822_SignOfTheProductOfAnArray.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+/**
+ * @author ycb
+ * @date 2022/3/20-20:25
+ */
+public class Problem_1822_SignOfTheProductOfAnArray {
+
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for (int n : nums) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n < 0) {
+                count++;
+            }
+        }
+        return (count & 1) == 0 ? 1 : -1;
+    }
+}
