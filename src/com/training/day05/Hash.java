8d604f731e91ef4840b751a11a00caeb5d13a39a
yangchangbin <1042397946@qq.com>
2022-04-02 12:42:04 +0800

    每日一题

--- src/com/training/day05/Hash.java
+++ src/com/training/day05/Hash.java
@@ -1,27 +1,27 @@
-package com.training.day05;
-
-import javax.xml.bind.DatatypeConverter;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-/**
- * @author ycb
- * @since 2021/8/8-14:02
- */
-public class Hash {
-
-    private MessageDigest hash;
-
-    public Hash(String algorithm) {
-        try {
-            hash = MessageDigest.getInstance(algorithm);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String hashCode(String input) {
-        return DatatypeConverter.printHexBinary(hash.digest(input.getBytes())).toUpperCase();
-    }
-
-}
+//package com.training.day05;
+//
+//import javax.xml.bind.DatatypeConverter;
+//import java.security.MessageDigest;
+//import java.security.NoSuchAlgorithmException;
+//
+///**
+// * @author ycb
+// * @since 2021/8/8-14:02
+// */
+//public class Hash {
+//
+//    private MessageDigest hash;
+//
+//    public Hash(String algorithm) {
+//        try {
+//            hash = MessageDigest.getInstance(algorithm);
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public String hashCode(String input) {
+//        return DatatypeConverter.printHexBinary(hash.digest(input.getBytes())).toUpperCase();
+//    }
+//
+//}
