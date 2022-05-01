9a7d856b10b6f8739b2f24dff1cdf5d3eb13c735
yangchangbin <1042397946@qq.com>
2022-05-01 13:46:05 +0800

    每日一题

--- src/com/leetcode/Problem_0131_PalindromePartitioning.java
+++ src/com/leetcode/Problem_0131_PalindromePartitioning.java
@@ -9,37 +9,45 @@ import java.util.List;
  */
 public class Problem_0131_PalindromePartitioning {
 
-    public static List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            List<String> cnt = new ArrayList<>();
-            for (int j = i; j < s.length(); j++) {
-                String cur = s.substring(i, j + 1);
-                if (isPalindrome(cur)) {
-                    cnt.add(cur);
-                }
-            }
-            ans.add(cnt);
-        }
-        return ans;
+    public int[][] f;
+    public List<List<String>> ret = new ArrayList<>();
+    public List<String> ans = new ArrayList<>();
+    public int n;
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new int[n][n];
+
+        dfs(s, 0);
+        return ret;
     }
 
-    public static boolean isPalindrome(String str) {
-        int l = 0;
-        int r = str.length() - 1;
-        while (l <= r) {
-            if (str.charAt(l) != str.charAt(r)) {
-                return false;
-            }
-            l++;
-            r--;
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<>(ans));
+            return;
         }
-        return true;
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(s, i, j) == 1) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        String str = "ada";
-        boolean ans = isPalindrome(str);
-        System.out.println(ans);
+    // 记忆化搜索中，f[i][j] = 0 表示未搜索，1 表示是回文串，-1 表示不是回文串
+    public int isPalindrome(String s, int i, int j) {
+        if (f[i][j] != 0) {
+            return f[i][j];
+        }
+        if (i >= j) {
+            f[i][j] = 1;
+        } else if (s.charAt(i) == s.charAt(j)) {
+            f[i][j] = isPalindrome(s, i + 1, j - 1);
+        } else {
+            f[i][j] = -1;
+        }
+        return f[i][j];
     }
 }
