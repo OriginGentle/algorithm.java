25e3ebcf6362020b8515f0ede3217798339704c2
yangchangbin <1042397946@qq.com>
2022-05-13 08:45:29 +0800

    每日一题

---
+++ src/com/leetcode/interview/Problem_01_05_OneAwayLcci.java
@@ -0,0 +1,46 @@
+package com.leetcode.interview;
+
+/**
+ * @author ycb
+ * @date 2022/5/13-08:24
+ */
+public class Problem_01_05_OneAwayLcci {
+
+    public static boolean oneEditAway(String first, String second) {
+        if (first == null || second == null) {
+            return false;
+        }
+        int n = first.length();
+        int m = second.length();
+        if (Math.abs(n - m) > 1) {
+            return false;
+        }
+        boolean used = false;
+        for (int i = 0, j = 0; i < n && j < m; ) {
+            if (first.charAt(i) == second.charAt(j)) {
+                i++;
+                j++;
+            } else if (used) {
+                return false;
+            } else {
+                if (m > n) {
+                    j++;
+                } else if (m < n) {
+                    i++;
+                } else {
+                    i++;
+                    j++;
+                }
+                used = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String f = "pale";
+        String s = "ple";
+        boolean ans = oneEditAway(f, s);
+        System.out.println(ans);
+    }
+}
