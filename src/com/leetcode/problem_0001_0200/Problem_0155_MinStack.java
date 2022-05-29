796f6570240bd064f3dd5537950597b8b8d158f8
yangchangbin <1042397946@qq.com>
2022-05-29 19:28:33 +0800

    周赛第四题

---
+++ src/com/leetcode/problem_0001_0200/Problem_0155_MinStack.java
@@ -0,0 +1,47 @@
+package com.leetcode.problem_0001_0200;
+
+import java.util.Stack;
+
+/**
+ * @author ycb
+ * @date 2022/5/29-19:24
+ */
+public class Problem_0155_MinStack {
+
+    public static class MinStack {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MinStack() {
+            stackData = new Stack<>();
+            stackMin = new Stack<>();
+        }
+
+        public void push(int value) {
+            if (stackMin.isEmpty()) {
+                stackMin.push(value);
+            } else if (value <= this.getMin()) {
+                stackMin.push(value);
+            }
+            this.stackData.push(value);
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            int value = this.stackData.pop();
+            if (value == this.getMin()) {
+                this.stackMin.pop();
+            }
+            return value;
+        }
+
+        public int getMin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            return this.stackMin.peek();
+        }
+    }
+}
--- src/com/leetcode/problem_biweekly/contest_79/Solution_4.java
+++ src/com/leetcode/problem_biweekly/contest_79/Solution_4.java
@@ -13,51 +13,71 @@ public class Solution_4 {
 
     public static class BookMyShow {
 
-        public int row;
-        public int col;
-        public HashMap<Integer, Integer> rowInfo;
+        int n, m;
+        int[] min;
+        long[] sum;
 
         public BookMyShow(int n, int m) {
-            row = n;
-            col = m;
-            rowInfo = new HashMap<>();
-            for (int i = 0; i < row; i++) {
-                rowInfo.put(i, 0);
-            }
+            this.n = n;
+            this.m = m;
+            min = new int[n * 4];
+            sum = new long[n * 4];
         }
 
         public int[] gather(int k, int maxRow) {
-            for (int i = 0; i <= maxRow; i++) {
-                int start = rowInfo.get(i);
-                // 1 1 1 1 1 1 1
-                // 0 1 2 3 4 5 6
-                // 0 0 0
-                if (start + k - 1 < col) {
-                    int[] ans = new int[]{i, start};
-                    rowInfo.put(i, start + k);
-                    return ans;
-                }
-            }
-            return new int[]{};
+            int i = index(1, 1, n, maxRow + 1, m - k);
+            if (i == 0) return new int[]{}; // 不存在
+            var seats = (int) query_sum(1, 1, n, i, i);
+            add(1, 1, n, i, k); // 占据 k 个座位
+            return new int[]{i - 1, seats};
         }
 
         public boolean scatter(int k, int maxRow) {
-            for (int i = 0; i <= maxRow; i++) {
-                Integer start = rowInfo.get(i);
-                // 1 1 1 1 1 1 1
-                // 0 1 2 3 4 5 6
-                // 1 1 1
-                int can = col - start + 1;
-                if (can <= 0) {
-                    continue;
-                }
-                k -= can;
-                rowInfo.put(i, start + k);
-                if (k <= 0) {
+            if ((long) (maxRow + 1) * m - query_sum(1, 1, n, 1, maxRow + 1) < k) return false; // 剩余座位不足 k 个
+            // 从第一个没有坐满的排开始占座
+            for (var i = index(1, 1, n, maxRow + 1, m - 1); ; ++i) {
+                var left_seats = m - (int) query_sum(1, 1, n, i, i);
+                if (k <= left_seats) { // 剩余人数不够坐后面的排
+                    add(1, 1, n, i, k);
                     return true;
                 }
+                k -= left_seats;
+                add(1, 1, n, i, left_seats);
             }
-            return false;
+        }
+
+        // 将 idx 上的元素值增加 val
+        void add(int o, int l, int r, int idx, int val) {
+            if (l == r) {
+                min[o] += val;
+                sum[o] += val;
+                return;
+            }
+            var m = (l + r) / 2;
+            if (idx <= m) add(o * 2, l, m, idx, val);
+            else add(o * 2 + 1, m + 1, r, idx, val);
+            min[o] = Math.min(min[o * 2], min[o * 2 + 1]);
+            sum[o] = sum[o * 2] + sum[o * 2 + 1];
+        }
+
+        // 返回区间 [L,R] 内的元素和
+        long query_sum(int o, int l, int r, int L, int R) { // L 和 R 在整个递归过程中均不变，将其大写，视作常量
+            if (L <= l && r <= R) return sum[o];
+            var sum = 0L;
+            var m = (l + r) / 2;
+            if (L <= m) sum += query_sum(o * 2, l, m, L, R);
+            if (R > m) sum += query_sum(o * 2 + 1, m + 1, r, L, R);
+            return sum;
+        }
+
+        // 返回区间 [1,R] 中 <= val 的最靠左的位置，不存在时返回 0
+        int index(int o, int l, int r, int R, int val) { // R 在整个递归过程中均不变，将其大写，视作常量
+            if (min[o] > val) return 0; // 说明整个区间的元素值都大于 val
+            if (l == r) return l;
+            var m = (l + r) / 2;
+            if (min[o * 2] <= val) return index(o * 2, l, m, R, val); // 看看左半部分
+            if (m < R) return index(o * 2 + 1, m + 1, r, R, val); // 看看右半部分
+            return 0;
         }
     }
 }
