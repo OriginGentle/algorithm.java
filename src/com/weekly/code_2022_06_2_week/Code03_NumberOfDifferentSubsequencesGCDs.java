74268c3bfa0faf1260637dac0b446cd27b6d079c
yangchangbin <1042397946@qq.com>
2022-06-19 16:06:01 +0800

    weekly

---
+++ src/com/weekly/code_2022_06_2_week/Code03_NumberOfDifferentSubsequencesGCDs.java
@@ -0,0 +1,45 @@
+package com.weekly.code_2022_06_2_week;
+
+import javax.net.ssl.SNIHostName;
+
+/**
+ * @author ycb
+ * @date 2022/6/19-14:51
+ * @desc https://leetcode.com/problems/number-of-different-subsequences-gcds/
+ */
+public class Code03_NumberOfDifferentSubsequencesGCDs {
+
+    // n : 数组中的最大值
+    // n/1 + n/2 + n/3 + ... + n/n --> O(N * logN)
+    public static int countDifferentSubsequenceGCDs(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(num, max);
+        }
+        boolean[] cntSet = new boolean[max + 1];
+        for (int num : nums) {
+            cntSet[num] = true;
+        }
+        int ans = 0;
+        for (int a = 1; a <= max; a++) {
+            int m = a;
+            for (; m <= max; m += a) {
+                if (cntSet[m]) break;
+            }
+            for (int n = m; n <= max; n += a) {
+                if (cntSet[n]) {
+                    m = gcd(m, n);
+                    if (m == a) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int gcd(int m, int n) {
+        return n == 0 ? m : gcd(n, m % n);
+    }
+}
