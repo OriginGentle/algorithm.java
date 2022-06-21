9df236216870a4a33fb6e8a92f15362c313c8dc0
yangchangbin <1042397946@qq.com>
2022-06-21 09:54:17 +0800

    每日一题

---
+++ src/com/leetcode/problem_1001_1200/Problem_1108_DefangingAnIpAddress.java
@@ -0,0 +1,20 @@
+package com.leetcode.problem_1001_1200;
+
+/**
+ * @author ycb
+ * @date 2022/6/21-09:52
+ */
+public class Problem_1108_DefangingAnIpAddress {
+
+    public String defangIPaddr(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length(), idx = -1;
+        while (++idx < n) {
+            char c = s.charAt(idx);
+            if (c == '.') sb.append('[');
+            sb.append(c);
+            if (c == '.') sb.append(']');
+        }
+        return sb.toString();
+    }
+}
