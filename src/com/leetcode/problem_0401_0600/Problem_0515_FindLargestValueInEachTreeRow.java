fa860c32c1a2fd86fc091e83dd227a21ad9d5e0d
yangchangbin <1042397946@qq.com>
2022-06-24 10:40:35 +0800

    每日一题

---
+++ src/com/leetcode/problem_0401_0600/Problem_0515_FindLargestValueInEachTreeRow.java
@@ -0,0 +1,58 @@
+package com.leetcode.problem_0401_0600;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author ycb
+ * @date 2022/6/24-09:00
+ */
+public class Problem_0515_FindLargestValueInEachTreeRow {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.add(root);
+        int max = Integer.MIN_VALUE;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = deque.poll();
+                max = Math.max(cur.val, max);
+                if (cur.left != null) {
+                    deque.add(cur.left);
+                }
+                if (cur.right != null) {
+                    deque.add(cur.right);
+                }
+            }
+            res.add(max);
+            max = Integer.MIN_VALUE;
+        }
+        return res;
+    }
+}
