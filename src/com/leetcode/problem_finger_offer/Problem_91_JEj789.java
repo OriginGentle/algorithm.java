e37a720fd0547fdb986f053f29bdb0306bb2a56c
yangchangbin <1042397946@qq.com>
2022-06-25 21:23:20 +0800

    每日一题

---
+++ src/com/leetcode/problem_finger_offer/Problem_91_JEj789.java
@@ -0,0 +1,23 @@
+package com.leetcode.problem_finger_offer;
+
+/**
+ * @author ycb
+ * @date 2022/6/25-21:12
+ */
+public class Problem_91_JEj789 {
+
+    public static int minCost(int[][] costs) {
+        if (costs == null || costs[0].length == 0) {
+            return 0;
+        }
+        int n = costs.length;
+        int a = costs[0][0], b = costs[0][1], c = costs[0][2];
+        for (int i = 1; i < n; i++) {
+            int d = Math.min(b, c) + costs[i][0];
+            int e = Math.min(a, c) + costs[i][1];
+            int f = Math.min(a, b) + costs[i][2];
+            a = d; b = e; c = f;
+        }
+        return Math.min(a, Math.min(b, c));
+    }
+}
