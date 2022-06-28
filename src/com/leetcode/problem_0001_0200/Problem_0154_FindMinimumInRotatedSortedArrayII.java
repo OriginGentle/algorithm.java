04eefc023ecbc832c7d3a2525343c3aec4650d4d
yangchangbin <1042397946@qq.com>
2022-06-28 16:44:22 +0800

    每日一题

--- src/com/leetcode/problem_0001_0200/Problem_0154_FindMinimumInRotatedSortedArrayII.java
+++ src/com/leetcode/problem_0001_0200/Problem_0154_FindMinimumInRotatedSortedArrayII.java
@@ -3,12 +3,25 @@ package com.leetcode.problem_0001_0200;
 /**
  * @author ycb
  * @date 2022/6/24-14:07
- * @desc
  */
 public class Problem_0154_FindMinimumInRotatedSortedArrayII {
 
     public static int findMin(int[] nums) {
-
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (nums[m] < nums[r]) {
+                r = m;
+            } else if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r--;
+            }
+        }
+        return nums[l];
     }
 }
---
+++ src/com/leetcode/problem_0001_0200/Problem_0160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,60 @@
+package com.leetcode.problem_0001_0200;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author ycb
+ * @date 2022/6/28-15:45
+ */
+public class Problem_0160_IntersectionOfTwoLinkedLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        ListNode h1 = headA;
+        while (h1 != null) {
+            set.add(h1);
+            h1 = h1.next;
+        }
+
+        ListNode h2 = headB;
+        while (h2 != null) {
+            if (set.contains(h2)) {
+                return h2;
+            }
+            h2 = h2.next;
+        }
+        return null;
+    }
+
+    /*
+    ====================================================================================================================
+     */
+
+    public static ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode h1 = headA;
+        ListNode h2 = headB;
+
+        while (h1 != h2) {
+            h1 = h1 == null ? headB : h1.next;
+            h2 = h2 == null ? headA : h2.next;
+        }
+        return h1;
+    }
+}
