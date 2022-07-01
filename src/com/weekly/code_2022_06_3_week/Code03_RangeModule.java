ff4c15f7303fb3393b702b24b11271462cba468f
yangchangbin <1042397946@qq.com>
2022-07-01 19:18:30 +0800

    weekly

--- src/com/weekly/code_2022_06_3_week/Code03_RangeModule.java
+++ src/com/weekly/code_2022_06_3_week/Code03_RangeModule.java
@@ -12,13 +12,13 @@ import java.util.TreeMap;
  */
 public class Code03_RangeModule {
 
-    class RangeModule {
+    static class RangeModule1 {
 
         // 某个区间的开头是key，结尾是value
         // map里表示的区间，能合并就合并，且没有交集
-        private TreeMap<Integer, Integer> map;
+        private final TreeMap<Integer, Integer> map;
 
-        public RangeModule() {
+        public RangeModule1() {
             map = new TreeMap<>();
         }
 
@@ -152,6 +152,113 @@ public class Code03_RangeModule {
             Map<Integer, Integer> subMap = map.subMap(left, true, right, false);
             Set<Integer> set = new HashSet<>(subMap.keySet());
             map.keySet().removeAll(set);
+        }
+    }
+
+    /*
+    ====================================================================================================================
+     */
+
+    static class RangeModule2 {
+
+        DynamicSegmentTree dst;
+
+        public RangeModule2() {
+            dst = new DynamicSegmentTree(1000000001);
+        }
+
+        public void addRange(int left, int right) {
+            dst.update(left, right - 1, 1);
+        }
+
+        public boolean queryRange(int left, int right) {
+            return dst.query(left, right - 1) == right - left;
+        }
+
+        public void removeRange(int left, int right) {
+            dst.update(left, right - 1, 0);
+        }
+
+        static class Node {
+            public int sum;
+            public int change;
+            public boolean update;
+            public Node left;
+            public Node right;
+        }
+
+        static class DynamicSegmentTree {
+            public Node root;
+            public int size;
+
+            public DynamicSegmentTree(int max) {
+                root = new Node();
+                size = max;
+            }
+
+            private void pushUp(Node c) {
+                c.sum = c.left.sum + c.right.sum;
+            }
+
+            private void pushDown(Node p, int ln, int rn) {
+                if (p.left == null) {
+                    p.left = new Node();
+                }
+                if (p.right == null) {
+                    p.right = new Node();
+                }
+                if (p.update) {
+                    p.left.update = true;
+                    p.right.update = true;
+                    p.left.change = p.change;
+                    p.right.change = p.change;
+                    p.left.sum = p.change * ln;
+                    p.right.sum = p.change * rn;
+                    p.update = false;
+                }
+            }
+
+            public void update(int s, int e, int v) {
+                update(root, 1, size, s, e, v);
+            }
+
+            private void update(Node c, int l, int r, int s, int e, int v) {
+                if (s <= l && r <= e) {
+                    c.update = true;
+                    c.change = v;
+                    c.sum = v * (r - l + 1);
+                } else {
+                    int mid = (l + r) >> 1;
+                    pushDown(c, mid - l + 1, r - mid);
+                    if (s <= mid) {
+                        update(c.left, l, mid, s, e, v);
+                    }
+                    if (e > mid) {
+                        update(c.right, mid + 1, r, s, e, v);
+                    }
+                    pushUp(c);
+                }
+            }
+
+            public int query(int s, int e) {
+                return query(root, 1, size, s, e);
+            }
+
+            private int query(Node c, int l, int r, int s, int e) {
+                if (s <= l && r <= e) {
+                    return c.sum;
+                }
+                int mid = (l + r) >> 1;
+                pushDown(c, mid - l + 1, r - mid);
+                int ans = 0;
+                if (s <= mid) {
+                    ans += query(c.left, l, mid, s, e);
+                }
+                if (e > mid) {
+                    ans += query(c.right, mid + 1, r, s, e);
+                }
+                return ans;
+            }
         }
     }
 }
