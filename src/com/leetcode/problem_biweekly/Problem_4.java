763d6e1817ee6ba8f9c2dd2904ffef9b9c1c6137
yangchangbin <1042397946@qq.com>
2022-08-15 15:01:56 +0800

    每日一题

--- src/com/leetcode/problem_biweekly/Problem_4.java
+++ src/com/leetcode/problem_biweekly/Problem_4.java
@@ -1,23 +1,6 @@
 package com.leetcode.problem_biweekly;
 
 public class Problem_4 {
-
-    public long minimumReplacement(int[] nums) {
-        long ans = 0;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] > nums[i + 1]) {
-                int cnt = nums[i] / nums[i + 1] + (nums[i] % nums[i + 1] == 0 ? 0 : 1);
-                ans += cnt - 1;
-                nums[i] /= cnt;
-            }
-        }
-        return ans;
-    }
-
-    /*
-    ====================================================================================================================
-     */
-
     // n >= 1
     public static int countSpecialNumbers(int n) {
         // n 一共由几位数字组成
