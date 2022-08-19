2ed7fb0d8a677aa5fded441602aed565d2f6a26e
yangchangbin <1042397946@qq.com>
2022-08-19 09:33:23 +0800

    leetcode:1450

---
+++ src/com/leetcode/problem_1401_1600/Problem_1450_NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,43 @@
+package com.leetcode.problem_1401_1600;
+
+/**
+ * @author ycb
+ * @date 2022/8/19-08:25
+ */
+public class Problem_1450_NumberOfStudentsDoingHomeworkAtAGivenTime {
+
+    public int size = 10001;
+
+    public int[] cmt;
+
+    public int busyStudent1(int[] startTime, int[] endTime, int queryTime) {
+        cmt = new int[size];
+        int n = startTime.length;
+        for (int i = 0; i < n; i++) {
+            int start = startTime[i], end = endTime[i];
+            build(start, end);
+        }
+        return cmt[queryTime];
+    }
+
+    private void build(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            cmt[i]++;
+        }
+    }
+
+    /*
+    ====================================================================================================================
+     */
+
+    public int busyStudent2(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        int n = startTime.length;
+        for (int i = 0; i < n; i++) {
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
