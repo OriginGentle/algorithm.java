b743f1c7acfc79f9df3820f7acce1d0cecd3476c
yangchangbin <1042397946@qq.com>
2022-08-31 10:26:40 +0800

    leetcode:220,946

---
+++ src/com/leetcode/problem_0801_1000/Problem_0946_ValidateStackSequences.java
@@ -0,0 +1,24 @@
+package com.leetcode.problem_0801_1000;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author ycb
+ * @date 2022/8/31-09:30
+ */
+public class Problem_0946_ValidateStackSequences {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int n = pushed.length;
+        for (int i = 0, j = 0; i < n; i++) {
+            deque.addLast(pushed[i]);
+            while (!deque.isEmpty() && deque.peekLast() == popped[j]) {
+                deque.pollLast();
+                j++;
+            }
+        }
+        return deque.isEmpty();
+    }
+}
