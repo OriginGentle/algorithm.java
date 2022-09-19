648837dbe5b2d1a30ad722831968e5287caceaa7
yangchangbin <1042397946@qq.com>
2022-09-19 11:41:15 +0800

    modify code

--- src/com/leetcode/problem_0201_0400/Problem_0235_LowestCommonAncestorOfABinarySearchTree.java
+++ src/com/leetcode/problem_0201_0400/Problem_0235_LowestCommonAncestorOfABinarySearchTree.java
@@ -1,6 +1,5 @@
 package com.leetcode.problem_0201_0400;
 
-import com.weekly.code_2022_03_2_week.Code08_TimeNSpace1LowestCommonAncestor;
 
 /**
  * @author ycb
@@ -11,12 +10,12 @@ public class Problem_0235_LowestCommonAncestorOfABinarySearchTree {
 
     public static class TreeNode {
         public int val;
-        public Code08_TimeNSpace1LowestCommonAncestor.TreeNode left;
-        public Code08_TimeNSpace1LowestCommonAncestor.TreeNode right;
+        public TreeNode left;
+        public TreeNode right;
     }
 
-    public static Code08_TimeNSpace1LowestCommonAncestor.TreeNode lowestCommonAncestor1(Code08_TimeNSpace1LowestCommonAncestor.TreeNode head, Code08_TimeNSpace1LowestCommonAncestor.TreeNode p, Code08_TimeNSpace1LowestCommonAncestor.TreeNode q) {
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode cur = head;
+    public static TreeNode lowestCommonAncestor1(TreeNode head, TreeNode p, TreeNode q) {
+        TreeNode cur = head;
         while (true) {
             if (p.val < cur.val && q.val < cur.val)
                 cur = cur.left;
@@ -34,17 +33,17 @@ public class Problem_0235_LowestCommonAncestorOfABinarySearchTree {
     ====================================================================================================================
      */
 
-    public static Code08_TimeNSpace1LowestCommonAncestor.TreeNode lowestCommonAncestor2(Code08_TimeNSpace1LowestCommonAncestor.TreeNode head, Code08_TimeNSpace1LowestCommonAncestor.TreeNode o1, Code08_TimeNSpace1LowestCommonAncestor.TreeNode o2) {
+    public static TreeNode lowestCommonAncestor2(TreeNode head, TreeNode o1, TreeNode o2) {
         if (findFirst(o1.left, o1, o2) != null || findFirst(o1.right, o1, o2) != null) {
             return o1;
         }
         if (findFirst(o2.left, o1, o2) != null || findFirst(o2.right, o1, o2) != null) {
             return o2;
         }
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode leftAim = findFirst(head, o1, o2);
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode cur = head;
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode mostRight = null;
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode ans = null;
+        TreeNode leftAim = findFirst(head, o1, o2);
+        TreeNode cur = head;
+        TreeNode mostRight = null;
+        TreeNode ans = null;
         while (cur != null) {
             mostRight = cur.left;
             if (mostRight != null) {
@@ -70,9 +69,9 @@ public class Problem_0235_LowestCommonAncestorOfABinarySearchTree {
         return ans != null ? ans : (findFirst(leftAim.right, o1, o2) != null ? leftAim : head);
     }
 
-    public static boolean findLeftAim(Code08_TimeNSpace1LowestCommonAncestor.TreeNode head, Code08_TimeNSpace1LowestCommonAncestor.TreeNode leftAim) {
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode tail = reverseEdge(head);
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode cur = tail;
+    public static boolean findLeftAim(TreeNode head, TreeNode leftAim) {
+        TreeNode tail = reverseEdge(head);
+        TreeNode cur = tail;
         boolean ans = false;
         while (cur != null) {
             if (cur == leftAim) {
@@ -84,9 +83,9 @@ public class Problem_0235_LowestCommonAncestorOfABinarySearchTree {
         return ans;
     }
 
-    public static Code08_TimeNSpace1LowestCommonAncestor.TreeNode reverseEdge(Code08_TimeNSpace1LowestCommonAncestor.TreeNode from) {
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode pre = null;
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode next = null;
+    public static TreeNode reverseEdge(TreeNode from) {
+        TreeNode pre = null;
+        TreeNode next = null;
         while (from != null) {
             next = from.right;
             from.right = pre;
@@ -96,13 +95,13 @@ public class Problem_0235_LowestCommonAncestorOfABinarySearchTree {
         return pre;
     }
 
-    public static Code08_TimeNSpace1LowestCommonAncestor.TreeNode findFirst(Code08_TimeNSpace1LowestCommonAncestor.TreeNode head, Code08_TimeNSpace1LowestCommonAncestor.TreeNode o1, Code08_TimeNSpace1LowestCommonAncestor.TreeNode o2) {
+    public static TreeNode findFirst(TreeNode head, TreeNode o1, TreeNode o2) {
         if (head == null) {
             return null;
         }
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode cur = head;
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode mostRight = null;
-        Code08_TimeNSpace1LowestCommonAncestor.TreeNode first = null;
+        TreeNode cur = head;
+        TreeNode mostRight = null;
+        TreeNode first = null;
         while (cur != null) {
             mostRight = cur.left;
             if (mostRight != null) {
