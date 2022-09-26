69602517e892887c66bac444167376efb3e3b71a
yangchangbin <1042397946@qq.com>
2022-09-26 16:47:31 +0800

    leetcode:246,面试题17_19

---
+++ src/com/leetcode/problem_interview/Problem_17_19_MissingTwoLcci.java
@@ -0,0 +1,25 @@
+package com.leetcode.problem_interview;
+
+/**
+ * @author ycb
+ * @date 2022/9/26-13:55
+ */
+public class Problem_17_19_MissingTwoLcci {
+
+    public int[] missingTwo(int[] nums) {
+        int n = nums.length + 2;
+        int sum = n * (1 + n) / 2;
+
+        for (int x : nums)
+            sum -= x;
+
+        int m = sum / 2, rest = sum;
+        sum = m * (1 + m) / 2;
+
+        for (int num : nums) {
+            if (num <= m)
+                sum -= num;
+        }
+        return new int[]{sum, rest - sum};
+    }
+}
