dfadf48cbb400561f6c504b224b7adc35b1b3978
yangchangbin <1042397946@qq.com>
2022-09-27 08:32:36 +0800

    leetcode:面试题01_02

---
+++ src/com/leetcode/problem_interview/Problem_01_02_CheckPermutationLcci.java
@@ -0,0 +1,25 @@
+package com.leetcode.problem_interview;
+
+/**
+ * @author ycb
+ * @date 2022/9/27-08:23
+ */
+public class Problem_01_02_CheckPermutationLcci {
+
+    public boolean CheckPermutation(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        int[] map = new int[26];
+        for (char c : s1.toCharArray()) {
+            map[c - 'a']++;
+        }
+
+        for (char c : s2.toCharArray()) {
+            map[c - 'a']--;
+            if (map[c - 'a'] < 0)
+                return false;
+        }
+        return true;
+    }
+}
