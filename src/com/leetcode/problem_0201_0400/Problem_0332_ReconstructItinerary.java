3b4114c393a6de4f1fb66c9f88df678b4a1ed270
yangchangbin <1042397946@qq.com>
2022-11-03 16:26:20 +0800

    LeetCode:332

---
+++ src/com/leetcode/problem_0201_0400/Problem_0332_ReconstructItinerary.java
@@ -0,0 +1,34 @@
+package com.leetcode.problem_0201_0400;
+
+import java.util.*;
+
+/**
+ * @author ycb
+ * @date 2022/11/3-16:19
+ */
+public class Problem_0332_ReconstructItinerary {
+
+    public Map<String, PriorityQueue<String>> cmt = new HashMap<>();
+    public List<String> ans = new ArrayList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        for (List<String> t : tickets) {
+            String from = t.get(0), to = t.get(1);
+            if (!cmt.containsKey(from))
+                cmt.put(from, new PriorityQueue<>());
+            cmt.get(from).add(to);
+        }
+
+        dfs("JFK");
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    public void dfs(String cur) {
+        while (cmt.containsKey(cur) && cmt.get(cur).size() > 0) {
+            String next = cmt.get(cur).poll();
+            dfs(next);
+        }
+        ans.add(cur);
+    }
+}
