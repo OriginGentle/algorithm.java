68b96c55aadaf55ff457b03aaa2f03849d3d98ff
yangchangbin <1042397946@qq.com>
2022-11-17 21:21:09 +0800

    LeetCode:792

---
+++ src/com/leetcode/problem_0601_0800/Problem_0792_NumberOfMatchingSubsequences.java
@@ -0,0 +1,48 @@
+package com.leetcode.problem_0601_0800;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ycb
+ * @date 2022/11/17-21:11
+ */
+public class Problem_0792_NumberOfMatchingSubsequences {
+
+    public int numMatchingSubseq(String s, String[] words) {
+        int n = s.length(), ans = 0;
+        Map<Character, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> list = map.getOrDefault(s.charAt(i), new ArrayList<>());
+            list.add(i);
+            map.put(s.charAt(i), list);
+        }
+
+        for (String w : words) {
+            boolean ok = true;
+            int m = w.length(), idx = -1;
+            for (int i = 0; i < m && ok; i++) {
+                List<Integer> cmt = map.getOrDefault(w.charAt(i), new ArrayList<>());
+                int l = 0, r = cmt.size() - 1;
+                while (l < r) {
+                    int mid = (l + r) >> 1;
+                    if (cmt.get(mid) > idx)
+                        r = mid;
+                    else
+                        l = mid + 1;
+                }
+
+                if (r < 0 || cmt.get(r) <= idx)
+                    ok = false;
+                else
+                    idx = cmt.get(r);
+            }
+            if (ok)
+                ans++;
+        }
+        return ans;
+    }
+}
