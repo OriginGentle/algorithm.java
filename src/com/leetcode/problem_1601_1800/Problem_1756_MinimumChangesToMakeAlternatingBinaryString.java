5a75783a0d2add76dd5e8077f2e9f6329790fc76
yangchangbin <1042397946@qq.com>
2022-11-30 00:30:45 +0800

    LeetCode:895,1756

---
+++ src/com/leetcode/problem_1601_1800/Problem_1756_MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,16 @@
+package com.leetcode.problem_1601_1800;
+
+/**
+ * @author ycb
+ * @date 2022/11/29-09:51
+ */
+public class Problem_1756_MinimumChangesToMakeAlternatingBinaryString {
+
+    public static int minOperations(String s) {
+        int cnt = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
+            cnt += (s.charAt(i) - '0') ^ (i & 1);
+        }
+        return Math.min(cnt, n - cnt);
+    }
+}
