22c5265923532bac4c18099e924e75495cf1c769
yangchangbin <1042397946@qq.com>
2022-12-09 23:48:26 +0800

    LeetCode:1780

---
+++ src/com/leetcode/problem_1601_1800/Problem_1780_CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,17 @@
+package com.leetcode.problem_1601_1800;
+
+/**
+ * @author ycb
+ * @date 2022/12/9-23:36
+ */
+public class Problem_1780_CheckIfNumberIsASumOfPowersOfThree {
+
+    public static boolean checkPowersOfThree(int n) {
+        while (n != 0) {
+            if (n % 3 == 2)
+                return false;
+            n /= 3;
+        }
+        return true;
+    }
+}
