f5e4e2f8a7a1603dc7fe7f47e4a4375b10865641
yangchangbin <1042397946@qq.com>
2022-12-12 10:34:38 +0800

    LeetCode:1781

---
+++ src/com/leetcode/problem_1601_1800/Problem_1781_SumOfBeautyOfAllSubstrings.java
@@ -0,0 +1,28 @@
+package com.leetcode.problem_1601_1800;
+
+/**
+ * @author ycb
+ * @date 2022/12/12-10:09
+ */
+public class Problem_1781_SumOfBeautyOfAllSubstrings {
+
+    public static int beautySum(String s) {
+        int ans = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
+            int[] cnt = new int[26];
+            int maxFreq = 0;
+            for (int j = i; j < n; j++) {
+                char c = s.charAt(j);
+                cnt[c - 'a']++;
+                maxFreq = Math.max(maxFreq, cnt[c - 'a']);
+                int minFreq = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] > 0)
+                        minFreq = Math.min(minFreq, cnt[k]);
+                }
+                ans += maxFreq - minFreq;
+            }
+        }
+        return ans;
+    }
+}
