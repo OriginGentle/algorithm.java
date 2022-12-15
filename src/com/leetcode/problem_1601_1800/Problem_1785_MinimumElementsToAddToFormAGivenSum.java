65e81a30063b10584c75d8f252caf9e083c40957
yangchangbin <1042397946@qq.com>
2022-12-16 00:25:39 +0800

    LeetCode:1785

---
+++ src/com/leetcode/problem_1601_1800/Problem_1785_MinimumElementsToAddToFormAGivenSum.java
@@ -0,0 +1,18 @@
+package com.leetcode.problem_1601_1800;
+
+/**
+ * @author ycb
+ * @date 2022/12/16-00:22
+ */
+public class Problem_1785_MinimumElementsToAddToFormAGivenSum {
+
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        long diff = Math.abs(sum - goal);
+        return (int) ((diff + limit - 1) / limit);
+    }
+}
