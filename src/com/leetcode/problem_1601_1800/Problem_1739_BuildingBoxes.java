fa99f86b302c1bdf791aa8f881e3d5b4c4c0b8da
yangchangbin <1042397946@qq.com>
2022-12-25 22:36:46 +0800

    LeetCode:1739

---
+++ src/com/leetcode/problem_1601_1800/Problem_1739_BuildingBoxes.java
@@ -0,0 +1,40 @@
+package com.leetcode.problem_1601_1800;
+
+/**
+ * @author ycb
+ * @date 2022/12/25-22:25
+ */
+public class Problem_1739_BuildingBoxes {
+
+    public static int minimumBoxes1(int n) {
+        int ans = 0, maxN = 0;
+        for (int i = 1; maxN + ans + i <= n; i++) {
+            ans += i;
+            maxN += ans;
+        }
+
+        for (int j = 1; maxN < n; j++) {
+            ++ans;
+            maxN += j;
+        }
+
+        return ans;
+    }
+
+    /*
+    ====================================================================================================================
+     */
+
+    public static int minimumBoxes2(int n) {
+        int x = (int) Math.cbrt(6L * n);
+        int ans = x * (x + 1) / 2;
+        int maxN = (int) ((long) x * (x + 1) * (x + 2) / 6);
+        if (maxN > n) {
+            maxN -= ans;
+            ans -= x;
+        }
+
+        double sqrt = Math.sqrt(1 + 8 * (n - maxN));
+        return ans + (int) Math.ceil((-1 + sqrt) / 2);
+    }
+}
