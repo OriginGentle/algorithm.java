66b9fb7747461751f2f0837e274ddabb6219676a
yangchangbin <1042397946@qq.com>
2022-12-27 08:27:43 +0800

    LeetCode:2027

---
+++ src/com/leetcode/problem_2001_2200/Problem_2027_MinimumMovesToConvertString.java
@@ -0,0 +1,19 @@
+package com.leetcode.problem_2001_2200;
+
+/**
+ * @author ycb
+ * @date 2022/12/27-08:23
+ */
+public class Problem_2027_MinimumMovesToConvertString {
+
+    public static int minimumMoves(String s) {
+        int cover = -1, ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'X' && i > cover) {
+                ans++;
+                cover = i + 2;
+            }
+        }
+        return ans;
+    }
+}
