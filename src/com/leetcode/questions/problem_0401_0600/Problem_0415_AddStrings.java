932852ecce9212fe38def730db64a25ebb2f3989
yangchangbin <1042397946@qq.com>
2023-07-18 00:11:27 +0800

    调整字符串工具

--- src/com/leetcode/questions/problem_0401_0600/Problem_0415_AddStrings.java
+++ src/com/leetcode/questions/problem_0401_0600/Problem_0415_AddStrings.java
@@ -8,7 +8,7 @@ public class Problem_0415_AddStrings {
 
     public String addStrings(String num1, String num2) {
         int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         while (i >= 0 || j >= 0 || carry != 0) {
             int x = i >= 0 ? num1.charAt(i) - '0' : 0;
             int y = j >= 0 ? num2.charAt(j) - '0' : 0;
